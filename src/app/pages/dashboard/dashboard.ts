733fc67cebb7f9a41fe02732d422b3fe5eb33d01
beatrizlima-tech <bealima.tech@gmail.com>
2026-03-23 13:07:42 -0300

    feat: consulta de tarefas por data e exclusão de tarefas com ajustes no dashboard e telas relacionas

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -2,19 +2,35 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { Navbar } from '../../layout/navbar/navbar';
 import { Sidebar } from '../../layout/sidebar/sidebar';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Chart, ChartModule } from 'angular-highcharts';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     CommonModule,
     Navbar,
-    Sidebar
+    Sidebar,
+    FormsModule,
+    ReactiveFormsModule,
+    ChartModule
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
 
+  graficoDonut = signal<Chart>(new Chart());
+  graficoColunas = signal<Chart>(new Chart());
+
+  formulario = new FormGroup({
+    dataMin : new FormControl('', [Validators.required]),
+    dataMax : new FormControl('', [Validators.required]),
+  });
+
+  consultar(){
+    
+  }
  
 
 }
